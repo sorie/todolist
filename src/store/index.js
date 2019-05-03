39e0ce87892bbe0c25029339669b33cec0cbc814
sorie <enne3939@gmail.com>
2019-05-03 13:34:27 +0900

    add vuex

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Constant from '../Constant';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    todolist : [
+      { todo : "영화보기", done:false },
+      { todo : "주말 산책", done:false },
+      { todo : "ES6 학습", done:false },
+      { todo : "잠실야구장", done:false },
+    ]
+  },
+  mutations: {
+    [Constant.ADD_TODO] : (state, payload) => {
+         if (payload.todo !== "") {
+           state.todolist.push({ todo : payload.todo, done:false });
+         }
+    },
+    [Constant.DONE_TOGGLE] : (state, payload) => {
+      state.todolist[payload.index].done = !state.todolist[payload.index].do
+    },
+    [Constant.DELETE_TOTO] : (state, payload) => {
+      state.todolist.splice(payload.index,1);
+    }
+  }
+})
+
+export default store;
